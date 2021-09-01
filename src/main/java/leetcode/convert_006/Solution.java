8ca4eb2f83e9c196263dc69f9df8dc44c819ed40
longyanhong <longyanhong@mininglamp.com>
2021-09-01 18:03:51 +0800

    add 添加官方的写法

--- src/main/java/leetcode/convert_006/Solution.java
+++ src/main/java/leetcode/convert_006/Solution.java
@@ -1,5 +1,8 @@
 package leetcode.convert_006;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author longyh
  * @Description: 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
@@ -72,4 +75,28 @@ class Solution {
 
         return buffer.toString();
     }
+
+
+    public static String convert_office1(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
+
 }
