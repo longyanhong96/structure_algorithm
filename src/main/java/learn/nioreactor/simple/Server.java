d3eaff57c4d56ee74139c0002cd3a99c34838790
longyanhong <longyanhong@mininglamp.com>
2021-09-02 18:34:07 +0800

    add https://www.jianshu.com/p/188ef8462100的代码

---
+++ src/main/java/learn/nioreactor/simple/Server.java
@@ -0,0 +1,23 @@
+package learn.nioreactor.simple;
+
+/**
+ * @author longyh
+ * @Description:
+ * @analysis:
+ * @date 2021/9/2 2:48 下午
+ */
+public class Server {
+    Selector selector = new Selector();
+    Dispatcher eventLooper = new Dispatcher(selector);
+    Acceptor acceptor;
+
+    Server(int port) {
+        acceptor = new Acceptor(selector, port);
+    }
+
+    public void start() {
+        eventLooper.registEventHandler(EventType.ACCEPT, new AcceptEventHandler(selector));
+        new Thread(acceptor, "Acceptor-" + acceptor.getPort()).start();
+        eventLooper.handleEvents();
+    }
+}
